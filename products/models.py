81cfb8c5aecd86425ea91b42d1497c0d5332ccfa
rajababu3 <rbtheroot@gmail.com>
2016-09-18 12:47:24 +0530

    Added Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField(max_length=300)
+    price = models.DecimalField(decimal_places=2, max_digits=100)
+    slug = models.SlugField()
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    active = models.BooleanField(default=True)
+
+
+    def __unicode__(self):
+        return self.title
