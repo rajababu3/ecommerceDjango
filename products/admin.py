1e94852689545853734b5e4f7bfaa1c04e5d3f69
rajababu3 <rbtheroot@gmail.com>
2016-09-18 17:28:21 +0530

    Customized admin using admin.ModelAdmin

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from . models import Product
 # Register your models here.
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    date_hierarchy = 'timestamp'
+    search_fields = ['title', 'description']
+    list_filter = ['price', 'active']
+    list_display = ['__str__', 'title','price','active', 'updated']
+    list_editable = ['price','active']
+    readonly_fields = ['timestamp','updated']
+    prepopulated_fields = {"slug": ("title", )}
+
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
