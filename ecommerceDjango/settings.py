3319bf4bf6049a5e450b8a4efc08413006ddec1b
rajababu3 <rbtheroot@gmail.com>
2016-09-18 19:51:33 +0530

    Addec static files and changed static directory

--- ecommerceDjango/settings.py
+++ ecommerceDjango/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static', 'static_files'),
+)
--- products/models.py
+++ products/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(max_length=300)
     price = models.DecimalField(decimal_places=2, max_digits=100)
+    sale_price = models.DecimalField(decimal_places=2, max_digits=100, default=0.0, null=True, blank=True)
     slug = models.SlugField()
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
@@ -14,3 +15,14 @@ class Product(models.Model):
 
     def __str__(self):
         return str(self.title)
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product)
+    image = models.ImageField(upload_to='products/images/')
+    featured = models.BooleanField(default=False)
+    active = models.BooleanField(default=False)
+    thumbnail = models.BooleanField(default=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.product.title
