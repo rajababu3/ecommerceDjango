e2d4064724ea181d20f27a1e7146f17a27855379
rajababu3 <rbtheroot@gmail.com>
2016-09-19 19:59:46 +0530

    Added cart app and made cart.html

--- ecommerceDjango/settings.py
+++ ecommerceDjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
--- ecommerceDjango/urls.py
+++ ecommerceDjango/urls.py
@@ -21,6 +21,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('products.urls')),
+    url(r'', include('carts.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
