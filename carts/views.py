beb69c57fc29dd27e272771cc2273ff6c5ef161b
rajababu3 <rbtheroot@gmail.com>
2016-09-19 22:05:57 +0530

    Cart Update and Cart Total Functionalities

--- carts/views.py
+++ carts/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
+from products.models import Product
 from .models import Cart
 def view(request):
     cart = Cart.objects.all()[0]
     context = {'cart': cart}
     template = 'cart/view.html'
     return render(request, template, context)
+
+
+def update_cart(request, slug):
+    cart = Cart.objects.all()[0]
+    try:
+        product = Product.objects.get(slug=slug)
+    except product.DoesNotExist:
+        pass
+    except:
+        pass
+
+    if not product in cart.products.all():
+        cart.products.add(product)
+    else:
+        cart.products.remove(product)
+
+    new_total = 0.00
+    for item in cart.products.all():
+        new_total += float(item.price)
+    cart.total = new_total
+    cart.save()
+
+    return HttpResponseRedirect(reverse('cart'))
+
+
--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     url(r'^products/$', views.all, name='products'),
     #getting unique product link with slug
     url(r'^products/(?P<slug>[\w-]+)/$', views.single, name='single-product'),
-
     #url for search
-url(r'^s/$', views.search, name = 'search'),
+    url(r'^s/$', views.search, name = 'search'),
 ]
