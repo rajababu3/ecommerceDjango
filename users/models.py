3a39e25220f24adfa59346d16899324ae036df40
rajababu3 <rbtheroot@gmail.com>
2016-10-24 17:08:45 +0530

    Minor CSS Problem fixed

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User)
+    firstName = models.CharField(max_length=120)
+    lastName = models.CharField(max_length=120)
+    mobile = models.IntegerField(max_length=12)
+    address = models.CharField(max_length=250)
+
+    def __str__(self):
+        return str(self.user)
+
